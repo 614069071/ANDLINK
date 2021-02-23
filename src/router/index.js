89bcf22a93eec98df9f7b3c259c86fa7ffc67d7d
JinTong <tong.jin@cdatatec.com>
2021-02-23 13:44:03 +0800

    打包优化

--- src/router/index.js
+++ src/router/index.js
@@ -13,15 +13,10 @@ const routes = [
   {
     path: '/upload',
     name: 'upload',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/upload')
+    component: () => import(/* webpackChunkName: "upload" */ '../views/upload')
   }
 ]
 
-const router = new VueRouter({
-  routes
-})
+const router = new VueRouter({ routes })
 
 export default router
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,23 @@
+const TerserPlugin = require('terser-webpack-plugin')
+
 module.exports = {
   lintOnSave: false,
+  configureWebpack: config => { // eslint-disable-line
+    if (process.env.NODE_ENV === 'production') {
+      return {
+        optimization: {
+          minimizer: [
+            new TerserPlugin({
+              sourceMap: false,
+              terserOptions: {
+                compress: {
+                  drop_console: true
+                }
+              }
+            })
+          ]
+        }
+      }
+    }
+  }
 }
